f816108abab614e49a1117dd29aa20f49a6e811a
pawan779 <pawan.dharel77@gmail.com>
2019-12-24 16:40:48 +0545

    bottom navigation added

--- app/src/main/java/com/pawan/esoftwarica/MainActivity.java
+++ app/src/main/java/com/pawan/esoftwarica/MainActivity.java
@@ -1,8 +1,16 @@
 package com.pawan.esoftwarica;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.pawan.esoftwarica.fragments.AboutFragment;
+import com.pawan.esoftwarica.fragments.AddFragment;
+import com.pawan.esoftwarica.fragments.HomeFragment;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView=findViewById(R.id.buttomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+
+
+        //to view Home at start
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,new HomeFragment()).commit();
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener=
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selFragment=null;
+
+                    switch (menuItem.getItemId())
+                    {
+                        case R.id.nav_home:
+                            selFragment=new HomeFragment();
+                            break;
+                    }
+                    switch (menuItem.getItemId())
+                    {
+                        case R.id.nav_add:
+                            selFragment=new AddFragment();
+                            break;
+                    }
+                    switch (menuItem.getItemId())
+                    {
+                        case R.id.nav_about:
+                            selFragment=new AboutFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentContainer,
+                            selFragment).commit();
+
+                    return true;
+                }
+            };
 }
