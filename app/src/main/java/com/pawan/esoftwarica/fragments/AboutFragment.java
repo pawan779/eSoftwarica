4987d0dfa6918f057f0a88242abacef1cb5bfc93
pawan779 <pawan.dharel77@gmail.com>
2019-12-25 16:04:26 +0545

    add student design added

--- app/src/main/java/com/pawan/esoftwarica/fragments/AboutFragment.java
+++ app/src/main/java/com/pawan/esoftwarica/fragments/AboutFragment.java
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.pawan.esoftwarica.R;
 
@@ -16,17 +18,21 @@ import com.pawan.esoftwarica.R;
  */
 public class AboutFragment extends Fragment {
 
-
-    public AboutFragment() {
-        // Required empty public constructor
-    }
-
+private WebView webView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+                             Bundle savedInstanceState)
+    {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_about, container, false);
+        View view= inflater.inflate(R.layout.fragment_about, container, false);
+        WebView webView=(WebView) view.findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://softwarica.edu.np/");
+
+        return view;
+
     }
 
 }
